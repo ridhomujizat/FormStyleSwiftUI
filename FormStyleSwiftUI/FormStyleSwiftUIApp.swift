c1684491f9302c1eb665fdea8143cb2629b4e793
ridhotitipku <107243155+ridhotitipku@users.noreply.github.com>
2022-10-12 21:54:16 +0700

    Initial Commit

---
+++ FormStyleSwiftUI/FormStyleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormStyleSwiftUIApp.swift
+//  FormStyleSwiftUI
+//
+//  Created by ridho mujizat on 12/10/22.
+//
+
+import SwiftUI
+
+@main
+struct FormStyleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
